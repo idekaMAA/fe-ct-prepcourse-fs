f9b3c6364b83f3f9f9d62febb26830ce88f803ee
idekaMAA <aylen29@outlook.com>
2023-07-05 21:08:49 -0300

    ...

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -21,6 +21,7 @@ function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
+   //o simplemente return str + "!"
    var mensaje = str + "!"
    return mensaje
 }
